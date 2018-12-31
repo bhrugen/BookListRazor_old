302f3996ab7ce6013c022634416ba314fa6a6fec
Ben Spark <bhrugen1990@gmail.com>
2018-12-31 16:04:44 -0600

    Create Page Part 1

--- BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,9 @@ namespace BookListRazor.Pages.BookList
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public Book Book { get; set; }
+
         public void OnGet()
         {
 
