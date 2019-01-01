a8a021455636bb20fb5edc2cfe9cda603beee80f
Ben Spark <bhrugen1990@gmail.com>
2019-01-01 11:11:46 -0600

    Edit Get Handler

---
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet(int id)
+        {
+            Book = _db.Book.Find(id);
+        }
+    }
+}
