97f180a602148723846d28fe49b7eca80a8ca3f8
Ben Spark <bhrugen1990@gmail.com>
2019-01-01 10:26:14 -0600

    Create Post Handler

--- BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -10,12 +10,33 @@ namespace BookListRazor.Pages.BookList
 {
     public class CreateModel : PageModel
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
         [BindProperty]
         public Book Book { get; set; }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Book.Add(Book);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
